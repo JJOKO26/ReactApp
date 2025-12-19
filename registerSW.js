4963b6667c571b8c3e4201cae875b6ee99da1ee4
鈴木 智也 <suztomoy@fsi.co.jp>
2025-12-19 11:00:47 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ReactApp/sw.js', { scope: '/ReactApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
