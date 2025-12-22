42efa51abd11b9f357aa5ae23909da84ecd15bd9
鈴木 智也 <suztomoy@fsi.co.jp>
2025-12-22 09:20:09 +0900

    6 commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ReactApp">
       <App />
     </BrowserRouter>
   </StrictMode>
